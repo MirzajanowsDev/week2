3d95309fdbcf5e5e15677fd6ddd58412f4c0a5ee
TM <farohidimirzajonov@gmail.com>
2023-10-26 17:09:54 +0600

    first commit

---
+++ searchInsert.js
@@ -0,0 +1,19 @@
+function searchinsert(n){
+    let arr = [1, 3, 5, 6]
+    for (let index = 0; index <= arr.length; index++) {
+        if (n == arr[index]) {
+            return arr.indexOf(n)
+        }
+        else{
+            arr.push(n)
+            arr.sort((a,b) => a-b)
+            console.log(arr);
+            return arr.indexOf(n)
+        }
+        
+    }
+}
+console.log(
+
+    searchinsert(9)
+    );
